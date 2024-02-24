cb2bef0cf831b7f26dcfd156a646e26cfbc1108c
ilhamzik <zikriilham2@gmail.com>
2024-02-24 19:44:05 +0700

    Commit CarService.java and CarServiceImpl.java

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -0,0 +1,48 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    @Autowired
+    private CarRepository carRepo;
+
+
+    @Override
+    public Car create(Car car) {
+        carRepo.create(car);
+        return car;
+    }
+
+    @Override
+    public List<Car> findAll() {
+        Iterator<Car> carIterator = carRepo.findAll();
+        List<Car> allCar = new ArrayList<>();
+        carIterator.forEachRemaining(allCar::add);
+        return allCar;
+    }
+
+    @Override
+    public Car findById(String carId) {
+        Car car = carRepo.findById(carId);
+        return car;
+    }
+
+    @Override
+    public void update(String carId, Car car) {
+        carRepo.update(carId, car);
+    }
+
+    @Override
+    public void deleteCarById(String carId) {
+        carRepo.delete(carId);
+    }
+}
